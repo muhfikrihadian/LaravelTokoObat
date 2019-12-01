f2664d322abaa415a98cbfd801fc16ca109f2792
MuhFikriHadian <muhfikrihadian@gmail.com>
2019-12-01 20:59:21 +0700

    Second Commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,18 +7,38 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Toko Obat</div>
                 <div class="panel-body">
-                <button type="button" class="btn btn-primary col-md-12">Tambah Obat</button>
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    Selamat Datang !
-
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://www.watsons.co.id/medias/sys_master/front/zoom/8890369212446.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Mylanta</h5>
+                        <p class="card-text">Obat Maag</p>
+                        <p class="card-text">Stok : 10</p>
+                        <a href="#" class="btn btn-primary">Beli</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    @if (session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+    @endif
+
+    Selamat Datang !
+
 </div>
-@endsection
+</div>
+</div>
+</div>
+</div>
+<script type="text/javascript">
+    $('#exampleModal').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget
+          var recipient = button.data('whatever')
+          var modal = $(this) 
+          modal.find('.modal-title').text('New message to ' + recipient)
+          modal.find('.modal-body input').val(recipient)
+      })
+  </script>
+  @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,19 +69,25 @@
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="{{ url('/') }}">Toko Obat</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="{{ url('/home') }}">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{ url('/tambahobat') }}">Tambah Obat</a>
-            </li>
-            <li class="nav-item dropdown">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="{{ url('/home') }}">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/tambahobat') }}">Tambah Obat</a>
+                </li>
+                <li class="nav-item">
+                    <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="right" title="Tooltip on right">
+                      Tooltip on right
+                  </button>
+              </li>
+
+              <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   Dropdown
               </a>
@@ -109,5 +115,14 @@
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    $('#exampleModal').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget
+          var recipient = button.data('whatever')
+          var modal = $(this)
+          modal.find('.modal-title').text('New message to ' + recipient)
+          modal.find('.modal-body input').val(recipient)
+      })
+  </script>
 </body>
 </html>
